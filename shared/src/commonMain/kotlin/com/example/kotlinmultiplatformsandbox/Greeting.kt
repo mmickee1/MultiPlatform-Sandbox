7ba714bd395c704179f837b92f19047bcfdef8f1
Mikael Meinander <mikael.meinander@qvik.fi>
2023-10-18 11:03:18 +0300

    test java library code, doesnt work, update to kotlin library, following instructions kotlinlang.org

--- shared/src/commonMain/kotlin/com/example/kotlinmultiplatformsandbox/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/kotlinmultiplatformsandbox/Greeting.kt
@@ -1,9 +1,13 @@
 package com.example.kotlinmultiplatformsandbox
 
+import kotlin.random.Random
+
+
 class Greeting {
     private val platform: Platform = getPlatform()
 
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
+    fun greet(): List<String> = buildList {
+        add(if (Random.nextBoolean()) "Hi!" else "Hello!")
+        add("Guess what it is! > ${platform.name.reversed()}!")
     }
 }
